8fb413a3b0d61db75ff2f50302ed6c2fcac5ecea
michael-torkaman <michael.torkaman.t@gmail.com>
2024-05-07 11:30:31 -0700

    initail commit

---
+++ wwwroot/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ContosoCrafts.WebSite.Models;
+
+public class Product
+{
+    public string Identifier { get; set; }
+    public string Maker { get; set; }
+    
+    [JsonPropertyName("img")]
+    public string Image { get; set; }
+    public string Url { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public override ToString() => JsonSerializer.Serialize<Product>(this);
+    
+}
